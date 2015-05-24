2fd913bf9eda8ac86c4fa72790344b7ebb3e115c
Patrick Dinh <d.conghoang@gmail.com>
2015-05-24 21:25:04 +1000

    first commit

---
+++ LuceneSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuceneSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var searcher = new Searcher();
+
+            searcher.ClearLuceneIndex();
+            searcher.AddUpdateLuceneIndex(SampleDataRepository.GetAll());
+
+            var data = searcher.Search("City\\ in");
+
+            foreach (var row in data)
+            {
+                Console.WriteLine(row.ToString());
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
